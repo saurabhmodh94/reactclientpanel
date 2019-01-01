d18cf7c65fbdd7b3fe62d0c88dd8d03f78d945fe
Saurabh Modh <saurabhmodh94@gmail.com>
2019-01-01 21:06:09 +0530

    init setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import AppNavbar from './components/layout/AppNavbar';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <AppNavbar />
+            <h1>Hello World</h1>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
